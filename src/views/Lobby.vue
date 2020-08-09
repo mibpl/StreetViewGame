65c70c3f36e090d081f1ac25300c10ef3e6dbc1b
anula <anula@users.noreply.github.com>
2020-08-09 15:35:53 +0200

    Create basic Lobby view, non-functional yet

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,5 +1,31 @@
 <template>
-  <span>Lobby view</span>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="5">
+        <v-card outlined>
+          <v-card-text v-if="connected_players_names.length == 0">
+            <div>No one has joined yet...</div>
+          </v-card-text>
+          <v-list dense v-if="connected_players_names.length > 0">
+            <v-subheader>Connected Players</v-subheader>
+            <v-list-item
+              v-for="player_name in connected_players_names"
+              :key="player_name"
+            >
+              <v-list-item-title>{{ player_name }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+          <v-card-actions v-if="isChief()">
+            <v-spacer />
+            <v-btn color="primary">Start game</v-btn>
+          </v-card-actions>
+          <v-card-text v-if="!isChief()">
+            <div>Waiting for {{ chief }} to start the game...</div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -9,6 +35,17 @@
 
 export default {
   name: 'Lobby',
+  data: function() {
+    return {
+      connected_players_names: [],
+      chief: 'Pluto',
+    };
+  },
+  methods: {
+    isChief() {
+      return true;
+    },
+  },
   components: {},
 };
 </script>
