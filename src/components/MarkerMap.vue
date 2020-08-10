2fee49a26d5b734fc7c2a0d8806a5e53e9cf2345
mibpl <3181064+mibpl@users.noreply.github.com>
2020-08-09 18:28:58 -0700

    marker map

---
+++ src/components/MarkerMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="width: 800px; height: 800px">
+    <p style="text-decoration: underline">Baaa!</p>
+    <div id="map-anchor" style="width: 800px'; height: 800px" />
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: {},
+  data: function() {
+    return {};
+  },
+  name: 'MarkerMap',
+  mounted: function() {
+    const position = new google.maps.LatLng(37.86926,-122.254811);
+    const map = new google.maps.Map(document.getElementById('map-anchor'), {
+      zoom: 4,
+      center: new google.maps.LatLng(0,0)
+    });
+
+    const marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: 'Hello World!'
+    });
+  },
+};
+</script>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,17 +2,20 @@
   <div>
     <span>Game view</span>
     <Streets />
+    <MarkerMap />
   </div>
 </template>
 
 <script>
 // This is the main view for the actual game.
 import Streets from '@/components/Streets.vue';
+import MarkerMap from '@/components/MarkerMap.vue';
 
 export default {
   name: 'Game',
   components: {
     Streets,
+    MarkerMap
   },
 };
 </script>
