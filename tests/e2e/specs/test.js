5b1c564c77ab81109462b2bfcd1a0db53d38a460
anula <anula@users.noreply.github.com>
2020-08-16 19:49:55 +0200

    Stub firebase auth in Cypress test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,4 +1,13 @@
 // https://docs.cypress.io/api/introduction/api.html
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+beforeEach(() => {
+  cy.stub(firebase, 'auth').returns({
+    signInAnonymously: cy.stub().resolves(),
+    onAuthStateChanged: cy.stub(),
+  });
+});
 
 describe('Main view', () => {
   it('propagates username from cookie to root page', () => {
