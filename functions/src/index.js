85096f074827f6be0e6e10aaccc49502b599d4f9
Anna Szybalska <anula@users.noreply.github.com>
2021-02-16 19:11:27 +0100

    Remove functions/ folder and update emulators' defaults

--- functions/src/index.js
+++
@@ -1,7 +0,0 @@
-import { firestore } from 'firebase-functions';
-
-export const mibHelloWorld = firestore
-  .document('/v3/')
-  .onWrite((change, context) => {
-    console.log(change, context);
-  });
